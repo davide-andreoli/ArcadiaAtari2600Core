dc0dc9f73503119185eabcc57123e93c0aa00735
Davide Andreoli <davide.andreoli@icloud.com>
2024-07-16 18:54:23 +0200

    Initial Commit

---
+++ ArcadiaAtari2600Core/ArcadiaAtari2600Core.h
@@ -0,0 +1,18 @@
+//
+//  ArcadiaAtari2600Core.h
+//  ArcadiaAtari2600Core
+//
+//  Created by Davide Andreoli on 16/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArcadiaAtari2600Core.
+FOUNDATION_EXPORT double ArcadiaAtari2600CoreVersionNumber;
+
+//! Project version string for ArcadiaAtari2600Core.
+FOUNDATION_EXPORT const unsigned char ArcadiaAtari2600CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArcadiaAtari2600Core/PublicHeader.h>
+
+
